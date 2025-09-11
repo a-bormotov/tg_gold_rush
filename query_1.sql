a291f56ec0d3d0ca580a5e55e1859e95bc428b7b
Aleksei Bormotov <epiqweel@gmail.com>
2025-09-11 12:27:39 +0200

    q1 optimization

--- query_1.sql
+++ query_1.sql
@@ -1,18 +1,13 @@
-WITH agg AS (
-  SELECT ur."userId", SUM(ur.amount) AS gold
-  FROM users_resources_total ur
-  WHERE ur."resourceType" = 'gold'
-  GROUP BY ur."userId"
-)
 SELECT 
   CASE 
     WHEN u.username = 'Secret Dino' OR u.username IS NULL OR u.username = '' 
-      THEN a."userId"::text
+      THEN ur."userId"::text
     ELSE u.username
   END AS username,
-  a.gold,
-  a."userId"
-FROM agg a
-LEFT JOIN users u ON u.id = a."userId"
-order by a.gold desc 
-limit 1000
+  ur.amount AS gold,
+  ur."userId"
+FROM users_resources_total ur
+LEFT JOIN users u ON u.id = ur."userId"
+WHERE ur."resourceType" = 'gold'
+ORDER BY ur.amount DESC 
+LIMIT 1000
