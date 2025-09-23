565c98981d083342b07b280e08c5fbad5c741310
Aleksei Bormotov <epiqweel@gmail.com>
2025-09-23 18:33:32 +0200

    fix line

--- user_data.sql
+++ user_data.sql
@@ -13,6 +13,6 @@ FROM ids
 JOIN users u
   ON u.id::text = ids.id
 WHERE
-  u."createdAt"::date <= DATE '2025-09-12'   -- поставь нужную дату (включительно)
-  AND ids.id NOT ILIKE 'line%'               -- исключаем ID, начинающиеся на "line"
+  u."createdAt" < TIMESTAMP '2025-09-13 00:00:00'   -- поставь нужную дату (включительно)
+  AND lower(left(ids.id, 4)) <> 'line'       -- исключаем id, начинающиеся с "line"
 ORDER BY ids.ord;
